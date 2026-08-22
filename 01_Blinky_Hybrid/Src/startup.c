3083bc3b962f7323b70a22e988d271127db51181
danyy1l <danyyilshykerynets@gmail.com>
2026-08-22 13:51:32 +0200

    feat(core): implement bare-metal linker script andstartup code
    - Define memory layout (FLASH and SRAM) in custom linker script (.ld).
    - Implement _reset handler in C to initialize C runtime (CRT).
    - Add block copy routine for .data section (LMA to VMA).
    - Add zero-fill routine for .bss section.
    - Define ARM Cortex-M4 vector table with 113 IRQ entries (16 core + 97 MCU).
    - Use fixed-width integer types (uint32_t) for memory address manipulation.

--- 01_Blinky_Hybrid/Src/startup.c
+++ 01_Blinky_Hybrid/Src/startup.c
@@ -1,11 +1,19 @@
-int main(void) { return 0; }
+#include <stdint.h>
+
+/* Import of Linker Script symbols */
+extern uint32_t _sbss, _ebss, _sdata, _edata, _sidata;
+
+/* Declaration of _estack as a function is standard in firmware, as it avoids
+ * referencing with & or explicitly casting it */
+extern void _estack(void);
+
+/* Import of main function */
+extern int main(void);
 
 __attribute__((naked, noreturn)) void _reset(void) {
-  // memset .bss to zero and copy .data to RAM
-  extern long _sbss, _ebss, _sdata, _edata, _sidata;
-  for (long *dst = &_sbss; dst < &_ebss; dst++)
+  for (uint32_t *dst = &_sbss; dst < &_ebss; dst++)
     *dst = 0;
-  for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;)
+  for (uint32_t *dst = &_sdata, *src = &_sidata; dst < &_edata;)
     *dst++ = *src++;
 
   main();
@@ -14,3 +22,10 @@ __attribute__((naked, noreturn)) void _reset(void) {
   while (1)
     (void)0;
 }
+
+// 16 standard and 97 STM32-specific handlers
+__attribute__((section(".vectors"))) void (*const tab[16 + 97])(void) = {
+    _estack, /* Offset 0x00: Initial main stack pointer (MSP) */
+    _reset   /* Offset 0x04: Reset_Handler */
+             /* The other elements are initizalized to 0 by default */
+};
