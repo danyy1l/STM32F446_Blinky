a4069d44178d3cf38b3c5c0d60fd4ffb31edd54e
danyy1l <danyyilshykerynets@gmail.com>
2026-08-22 20:03:58 +0200

    feat(core): blink implementation
    -LED blinks correctly
    
    -Added varying blinking speed by pressing user button
    
    -Needs to be documented in README
    
    -Extension: Create my own register access and modification library

--- 01_Blinky_Hybrid/Src/main.c
+++ 01_Blinky_Hybrid/Src/main.c
@@ -8,6 +8,7 @@
  */
 
 #include "../Inc/stm32f446xx.h"
+#include <stdint.h>
 
 /**
  * First, I learn where the LED is on the board.
@@ -31,7 +32,67 @@
  * https://github.com/STMicroelectronics/cmsis-device-f4/tree/a833f4af71410f25b01468f976560d7ff63a2fc9/Include
  */
 
+typedef enum { VERY_SLOW = 0, SLOW, MEDIUM, FAST, TURBO } Blink_Speed;
+
 int main(void) {
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // Enable clock for PORT A
-  GPIOA->ODR &= GPIO_ODR_OD5_Pos; // Assert that PIN5 begins in off state (0V)
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+  GPIOA->MODER &= ~GPIO_MODER_MODE5; // Clear mode bits
+  GPIOA->MODER |=
+      GPIO_MODER_MODE5_0; // Activate bit 0, as 01 is output mode for moder
+  GPIOA->OTYPER &= ~GPIO_OTYPER_OT5; // Ensure port is in push-pull state
+                                     // instead of open drain
+  GPIOA->OSPEEDR &=
+      ~GPIO_OSPEEDER_OSPEEDR5; // Clear speed bits to set starting low speed
+
+  GPIOC->MODER &= ~GPIO_MODER_MODE13;
+  GPIOC->OTYPER &= ~GPIO_OTYPER_OT13;
+  GPIOC->OSPEEDR &=
+      ~GPIO_OSPEEDER_OSPEEDR13; // Clear speed bits to set starting low speed
+
+  Blink_Speed current_speed = VERY_SLOW;
+  volatile uint32_t count = 1000000U;
+  volatile uint8_t button_pressed = 0;
+  uint32_t ticks = 0;
+
+  while (1) {
+    if ((GPIOC->IDR & GPIO_IDR_ID13) == 0) {
+      if (button_pressed == 0) {
+        button_pressed = 1;
+
+        ++current_speed;
+        if (current_speed > TURBO)
+          current_speed = VERY_SLOW;
+
+        switch (current_speed) {
+        case VERY_SLOW:
+          count = 1000000U;
+          break;
+        case SLOW:
+          count = 500000U;
+          break;
+        case MEDIUM:
+          count = 250000U;
+          break;
+        case FAST:
+          count = 125000U;
+          break;
+        case TURBO:
+          count = 50000U;
+          break;
+        }
+      }
+    } else {
+      button_pressed = 0;
+    }
+
+    ++ticks;
+    if (ticks >= count) {
+      GPIOA->ODR ^= GPIO_ODR_OD5;
+      ticks = 0;
+    }
+  }
+
+  return 0;
 }
